652c807b0771d2516f348e6f19ebe8acfa57a48f
Adam Staněk <adam.stanek@ataccama.com>
2020-12-08 22:37:18 +0100

    reading the sensor data

--- src/websocket_handlers.go
+++ src/websocket_handlers.go
@@ -6,11 +6,56 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+type SensorInfoPayload struct {
+	Value      int32
+	ValueMilli int32
+	IsAlert    bool
+}
+
+type SensorUpdate struct {
+	Temperature *SensorInfoPayload
+	Humidity    *SensorInfoPayload
+}
+
+func processSensorData(sensorData []*SensorData) {
+	// Parse sensor update
+	sensorUpdate := &SensorUpdate{}
+	for _, sensorDataSet := range sensorData {
+		if *sensorDataSet.SensorType == SensorType_TEMPERATURE {
+			sensorUpdate.Temperature = &SensorInfoPayload{
+				Value:      *sensorDataSet.Value,
+				ValueMilli: *sensorDataSet.ValueMilli,
+				IsAlert:    *sensorDataSet.IsAlert,
+			}
+		} else if *sensorDataSet.SensorType == SensorType_HUMIDITY {
+			sensorUpdate.Humidity = &SensorInfoPayload{
+				Value:      *sensorDataSet.Value,
+				ValueMilli: *sensorDataSet.ValueMilli,
+				IsAlert:    *sensorDataSet.IsAlert,
+			}
+		}
+	}
+
+	if sensorUpdate.Humidity != nil || sensorUpdate.Temperature != nil {
+		msg := log.Debug()
+
+		if sensorUpdate.Temperature != nil {
+			msg.Float32("temperature", float32(sensorUpdate.Temperature.ValueMilli)/1000)
+		}
+
+		if sensorUpdate.Humidity != nil {
+			msg.Float32("humidity", float32(sensorUpdate.Humidity.ValueMilli)/1000)
+		}
+
+		msg.Msg("Received sensor data update")
+	}
+}
+
 func registerWebsocketHandlers(conn *WebsocketConnection, localStreamServer string) {
 
 	// Send initial set of requests upon successful connection
 	conn.OnReady(func(conn *WebsocketConnection) {
-		// Ask for sensor data
+		// Ask for sensor data (initial request)
 		conn.SendRequest(RequestType_GET_SENSOR_DATA, Request{
 			GetSensorData: &GetSensorData{
 				All: constRefBool(true),
@@ -41,7 +86,10 @@ func registerWebsocketHandlers(conn *WebsocketConnection, localStreamServer stri
 
 	// Listen for termination
 	termC := make(chan bool, 1)
-	conn.OnTermination(func() { termC <- true })
+	conn.OnTermination(func() {
+		// Closing the channel will broadcast to all the listeners, regardless to their number
+		close(termC)
+	})
 
 	// Keep-alive
 	conn.OnReady(func(conn *WebsocketConnection) {
@@ -60,5 +108,23 @@ func registerWebsocketHandlers(conn *WebsocketConnection, localStreamServer stri
 				}
 			}
 		}()
+	})
+
+	// Reading sensor data
+	conn.OnMessage(func(m *Message, conn *WebsocketConnection) {
+		// Sensor request initiated by us on start (or some other client, we don't care)
+		if *m.Type == Message_RESPONSE && m.Response != nil {
+			if *m.Response.RequestType == RequestType_GET_SENSOR_DATA && len(m.Response.SensorData) > 0 {
+				processSensorData(m.Response.SensorData)
+			}
+		} else
+
+		// Communication initiated from a cam
+		// Note: it sends the updates periodically on its own + whenever some significant change occurs
+		if *m.Type == Message_REQUEST && m.Request != nil {
+			if *m.Request.Type == RequestType_PUT_SENSOR_DATA && len(m.Request.SensorData_) > 0 {
+				processSensorData(m.Request.SensorData_)
+			}
+		}
 	})
 }
