19fd0b150309e7440b4a3bec3a992ff3699fd2a1
Alex Kestner <alex.kestner@gmail.com>
2021-07-04 16:14:54 -0700

    adds message type constants

--- pkg/client/const.go
+++ pkg/client/const.go
@@ -5,4 +5,8 @@ import "time"
 const (
 	// AuthTokenTimelife - Time duration after which we assume auth token expired
 	AuthTokenTimelife = 10 * time.Minute
+	// SoundEventMessageType is for working with sound event messages
+	SoundEventMessageType = "SOUND"
+	// MotionEventMessageType is for working with motion event messages
+	MotionEventMessageType = "MOTION"
 )
